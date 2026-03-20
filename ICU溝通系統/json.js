82e8482d539ad85c3977ed4dfe38cd52f7a93706
mikeOAO <mike28248010@gmail.com>
2026-03-20 12:56:29 +0800

    Delete ICU溝通系統 directory

--- ICU溝通系統/json.js
+++
@@ -1,9 +0,0 @@
-// 點擊卡片選中 / 再點取消
-function toggleSelect(card){
-  card.classList.toggle('selected');
-  if(card.classList.contains('selected')){
-    card.style.border = "3px solid #4CAF50";
-  } else {
-    card.style.border = "2px solid #ccc";
-  }
-}
